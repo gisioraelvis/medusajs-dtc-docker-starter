ef61a4973f164a0a82cde173d964cdd5caa0e60a
gisioraelvis <gisioraelvis20@gmail.com>
2026-05-21 11:17:27 +0300

    feat: add Redis configs

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     databaseDriverOptions: {
       ssl: false,
       sslmode: "disable",
@@ -17,6 +18,29 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: { redisUrl: process.env.REDIS_URL },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: { redis: { redisUrl: process.env.REDIS_URL } },
+    },
+    {
+      resolve: "@medusajs/medusa/locking",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/locking-redis",
+            id: "locking-redis",
+            is_default: true,
+            options: { redisUrl: process.env.REDIS_URL },
+          },
+        ],
+      },
+    },
+  ],
   admin: {
     vite: () => {
       return {
